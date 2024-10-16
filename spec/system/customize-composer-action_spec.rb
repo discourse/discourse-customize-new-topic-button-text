3144e6f8c31ae664286660a1d9d7b19645b3af4a
Kelvin Tan <kelv@discourse.org>
2024-10-16 20:22:55 +0800

    DEV: Update linting setup

--- spec/system/customize-composer-action_spec.rb
+++ spec/system/customize-composer-action_spec.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
 RSpec.describe "Customize New Topic Text - composer action text", system: true do
-  let!(:theme) do 
-    upload_theme_component
-  end
+  let!(:theme) { upload_theme_component }
 
   fab!(:user) { Fabricate(:user, refresh_auto_groups: true) }
 
-  before do 
-    sign_in(user)
-  end
+  before { sign_in(user) }
 
-  shared_examples "custom composer text" do |type, custom_text| 
+  shared_examples "custom composer text" do |type, custom_text|
     fab!(:category)
     fab!(:category2) { Fabricate(:category) }
     fab!(:tag)
     fab!(:tag2) { Fabricate(:tag) }
-  
+
     before do
       setting_type = type == "category" ? category.id : tag.name
-      theme.update_setting(:custom_new_topic_text, "[{\"filter\":\"#{setting_type}\",\"composer_action_text\":\"#{custom_text}\",\"composer_button_text\":\"#{custom_text}\"}]")
+      theme.update_setting(
+        :custom_new_topic_text,
+        "[{\"filter\":\"#{setting_type}\",\"composer_action_text\":\"#{custom_text}\",\"composer_button_text\":\"#{custom_text}\"}]",
+      )
       theme.save!
     end
-  
+
     it "the composer action and button text is custom" do
       visit_url_based_on_type(type, 1)
       find("#create-topic").click
@@ -34,9 +34,9 @@ RSpec.describe "Customize New Topic Text - composer action text", system: true d
       find("#create-topic").click
 
       expect(find(".action-title")).not_to have_content(custom_text)
-      expect(find(".save-or-cancel .create")).not_to have_content(custom_text)    
+      expect(find(".save-or-cancel .create")).not_to have_content(custom_text)
     end
-  
+
     def visit_url_based_on_type(type, id)
       if type == "category"
         category_to_visit = id == 1 ? category : category2
@@ -47,7 +47,7 @@ RSpec.describe "Customize New Topic Text - composer action text", system: true d
       end
     end
   end
-  
+
   describe "When customizing the composer text for a category" do
     include_examples "custom composer text", "category", "Bawk!"
   end
--- spec/system/customize-new-topic-button_spec.rb
+++ spec/system/customize-new-topic-button_spec.rb
@@ -1,41 +1,41 @@
+# frozen_string_literal: true
 RSpec.describe "Customize New Topic Text - new topic button", system: true do
-  let!(:theme) do 
-    upload_theme_component
-  end
+  let!(:theme) { upload_theme_component }
 
   fab!(:user) { Fabricate(:user, refresh_auto_groups: true) }
 
-  before do 
-    sign_in(user)
-  end
+  before { sign_in(user) }
 
-  shared_examples "custom new topic button" do |type, custom_text| 
+  shared_examples "custom new topic button" do |type, custom_text|
     fab!(:category)
     fab!(:category2) { Fabricate(:category) }
     fab!(:tag)
     fab!(:tag2) { Fabricate(:tag) }
-  
+
     before do
       setting_type = type == "category" ? category.id : tag.name
-      theme.update_setting(:custom_new_topic_text, "[{\"filter\":\"#{setting_type}\",\"button_text\":\"#{custom_text}\",\"icon\":\"question\"}]")
+      theme.update_setting(
+        :custom_new_topic_text,
+        "[{\"filter\":\"#{setting_type}\",\"button_text\":\"#{custom_text}\",\"icon\":\"question\"}]",
+      )
       theme.save!
     end
-  
+
     it "the new topic button has custom text" do
       visit_url_based_on_type(type, 1)
       expect(find("#custom-create-topic")).to have_content(custom_text)
     end
-  
+
     it "the new topic button has a custom icon" do
       visit_url_based_on_type(type, 1)
       expect(find("#custom-create-topic")).to have_css(".d-icon-question")
     end
-  
+
     it "the new topic button in a different #{type} is not custom" do
       visit_url_based_on_type(type, 2)
       expect(find("#create-topic .d-button-label")).to have_content("New Topic")
     end
-  
+
     def visit_url_based_on_type(type, id)
       if type == "category"
         category_to_visit = id == 1 ? category : category2
@@ -46,7 +46,7 @@ RSpec.describe "Customize New Topic Text - new topic button", system: true do
       end
     end
   end
-  
+
   describe "When customizing the new topic button for a category" do
     include_examples "custom new topic button", "category", "Bawk!"
   end
